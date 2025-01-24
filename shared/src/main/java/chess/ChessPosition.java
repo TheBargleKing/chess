9535f8af0c8a449c2baa989032ae0edcd40da0dd
Caleb Mitchell <Caleb.mitchell005@gmail.com>
2025-01-23 23:39:05 -0700

    Setting up even more basic parameters.

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -9,11 +9,11 @@ package chess;
 public class ChessPosition {
 
     private final int row;
-    private final int column;
+    private final int col;
 
     public ChessPosition(int row, int col) {
         this.row = row;
-        this.column = col;
+        this.col = col;
     }
 
     /**
@@ -29,6 +29,6 @@ public class ChessPosition {
      * 1 codes for the left row
      */
     public int getColumn() {
-        return column;
+        return col;
     }
 }
